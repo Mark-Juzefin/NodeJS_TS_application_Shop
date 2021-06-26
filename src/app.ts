3599a054f4a02a9a59ac824db66fd298fe523cdf
HILARION <hilarionium@gmail.com>
2021-06-26 10:04:53 +0300

    add Create and Read operations

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,48 @@
+import { Request, Response } from 'express';
+import { Casheir } from './models';
+
 const express = require('express');
 const db = require('./db.ts');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8088;
 const app = express();
 
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log('Server do work!');
+// getAllCasheirs
+app.get('/casheir', async (req: Request, res: Response) => {
+  const casheir = await db.query(
+    `select casheir.firstname, casheir.lastname, casheir.age, shift.shiftname, casheir.yearsofexperience, shop.shopname
+    from casheir
+    left join shift on casheir.shiftid = shift.id
+    left join shop on casheir.shopid = shop.id`,
+  );
+
+  res.json(casheir.rows);
 });
 
-app.get('/', async (req:any, res:any) => {
-  const listOfShops = await db.query('SELECT * FROM Shop');
+// addNewCasheir
+app.post('/casheir', async (req: Request, res: Response) => {
+  const {
+    firstname, lastname, age, shiftid, yearsofexperience, shopid,
+  }: Casheir = req.body;
 
-  console.log(listOfShops);
+  if (!firstname && !lastname && !age && !shiftid && !yearsofexperience && !shopid) {
+    res.status(400).send('invalid input data');
+  }
+  try {
+    const casheir = await db.query(`
+  insert into casheir (firstname, lastname, age, shiftid, yearsofexperience, shopid)
+  values 
+  ('${firstname}', '${lastname}', ${age}, ${shiftid}, ${yearsofexperience}, ${shopid})
+  returning id`);
 
-  res.json(listOfShops);
+    res.json(casheir.rows);
+  } catch (error) {
+    res.status(400).send('invalid input data');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log('Server do work!');
 });
---
+++ src/models.ts
@@ -0,0 +1,9 @@
+export interface Casheir {
+  id?: number
+  firstname: string
+  lastname: string
+  age: number
+  shiftid: number
+  yearsofexperience: number
+  shopid: number
+}
